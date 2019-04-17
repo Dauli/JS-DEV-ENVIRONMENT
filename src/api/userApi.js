10a0947a0edc339032cf72f469c0653eb13bc953
Alexis Ange <alexisangedauli@gmail.com>
2019-04-17 21:45:55 +0200

    Manipulating data via JSON Server

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -9,9 +9,22 @@ export function getUsers() {
   return get('users');
 }
 
+// delete functionlity
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+
 // support get here, but you can handle POST or delete here as well
 function get(url) {
   return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+// del function. we didn't use delete because its a reserve keyword in js
+function del (url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+  return fetch(request).then(onSuccess, onError);
 }
 
 // handling success
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import './index.css';
+
 // importing the public function that containt our API
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 // Populate table of users via API call
 getUsers().then(result => {
@@ -15,4 +17,18 @@ getUsers().then(result => {
     </tr>`
   });
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // Must use Array.from() to create a real array from a DOM collection.
+  // getElementsByClassName only returns an "array like" object
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    }
+  });
 });
